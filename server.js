9bbfbdf3aa6521b20eeeb50f2195f49d000dd392
cristof-ui <toper0072@gmail.com>
2025-07-09 12:59:34 +0800

    Update server.js

--- server.js
+++ server.js
@@ -29,8 +29,7 @@ app.post('/webflow-hook', async (req, res) => {
       phone_number: phone,
       submitted_at: new Date().toISOString(),
       form_url: 'https://jarvis-tree-experts.webflow.io/contact',
-      referrer: 'https://jarvis-tree-experts.webflow.io/contact',
-      referring_url: 'https://jarvis-tree-experts.webflow.io/contact'
+      referrer: 'https://jarvistreeexperts.com/wp-admin/edit.php?post_type=page'
     }
   };
   // Log the payload to verify structure
